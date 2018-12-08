0e5d64df486c3c8537b93cadf484cad063930e92
RaymondDashWu <raymond31670@hotmail.com>
2018-12-07 16:02:42 -0800

    [FIX] Users can now log back in

--- models/user.js
+++ models/user.js
@@ -35,6 +35,8 @@ UserSchema.pre("save", function(next) {
 
 // Need to use function to enable this.password to work.
 UserSchema.methods.comparePassword = function(password, done) {
+  console.log(password)
+  console.log(this.password)
   bcrypt.compare(password, this.password, (err, isMatch) => {
     done(err, isMatch);
   });
